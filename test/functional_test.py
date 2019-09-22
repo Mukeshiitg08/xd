61a5b306b44323b76c09d35f7cfa04200daa85e2
Mukeshiitg08 <54905944+Mukeshiitg08@users.noreply.github.com>
2019-09-22 16:29:54 +0530

    Add files via upload

---
+++ test/functional_test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+
+class BrowserTitleTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path=r'/opt/geckodriver')
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_to_check_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Jarvis', self.browser.title)
+        # self.fail('finish the test')
+
+    def test_to_check_user_input(self):
+        self.browser.get('http://localhost:8000')
+
+if __name__ == '__main__':
+    unittest.main()
